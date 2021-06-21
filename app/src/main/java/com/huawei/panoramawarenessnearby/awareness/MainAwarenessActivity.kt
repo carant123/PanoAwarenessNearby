296e316e021c3ed4e07adf9a7920ee3d6a74122a
carant123 <carant896@hotmail.com>
2021-06-20 21:55:58 -0500

    Agregar registro Broadcast

--- app/src/main/java/com/huawei/panoramawarenessnearby/awareness/MainAwarenessActivity.kt
+++ app/src/main/java/com/huawei/panoramawarenessnearby/awareness/MainAwarenessActivity.kt
@@ -16,6 +16,8 @@ import kotlinx.android.synthetic.main.activity_awareness.*
 
 class MainAwarenessActivity : BaseActivity() {
 
+    var barrierReceiver: LightBarrierReceiver? = null
+
     init {
         activityIdent = this
     }
@@ -28,6 +30,15 @@ class MainAwarenessActivity : BaseActivity() {
 
     override fun inicializar() {
 
+    }
+
+    private fun registrarBarreraBroadcast(): PendingIntent {
+        var BARRIER_RECEIVER_ACTION = baseContext.packageName + Constantes.LIHT_BARRIER_RECEIVER_ACTION
+        var intent = Intent(BARRIER_RECEIVER_ACTION)
+        var pendingIntent = PendingIntent.getBroadcast(this,0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        barrierReceiver = LightBarrierReceiver()
+        registerReceiver(barrierReceiver, IntentFilter(BARRIER_RECEIVER_ACTION))
+        return pendingIntent
     }
 
 }
