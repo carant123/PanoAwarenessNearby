baf02544ff27d0e9fa969fb477dd5a94d868c644
carant123 <carant896@hotmail.com>
2021-06-22 00:08:27 -0500

    Inicial WifiShare

--- app/src/main/java/com/huawei/panoramawarenessnearby/nearbyservice/wifishare/WifiShareActivity.kt
+++ app/src/main/java/com/huawei/panoramawarenessnearby/nearbyservice/wifishare/WifiShareActivity.kt
@@ -5,7 +5,7 @@ import com.huawei.panoramawarenessnearby.BaseActivity
 import com.huawei.panoramawarenessnearby.R
 import kotlinx.android.synthetic.main.activity_wifi_share.*
 
-class WifiShareActivity : BaseActivity(), View.OnClickListener {
+class WifiShareActivity : BaseActivity() {
 
     private var mWiFiShare: WiFiShareHelper? = null
     private val REQUEST_CODE_REQUIRED_PERMISSIONS = 1
@@ -13,29 +13,7 @@ class WifiShareActivity : BaseActivity(), View.OnClickListener {
     override fun getLayout(): Int = R.layout.activity_wifi_share
 
     override fun inicializar() {
-        inicializarBotones()
-        inicializarWifiShareHelper()
-        verificarPermisos()
-    }
 
-    private fun inicializarWifiShareHelper() {
-        mWiFiShare = WiFiShareHelper(baseContext)
-        mWiFiShare?.setViewToFill(listView, authCodeText)
-    }
-
-    private fun inicializarBotones() {
-        button_share_wifi.setOnClickListener(this)
-        button_connect_wifi.setOnClickListener(this)
-    }
-
-    override fun onClick(view: View) {
-        when(view.id) {
-            R.id.button_share_wifi -> mWiFiShare?.shareWiFiConfig()
-            R.id.button_connect_wifi -> mWiFiShare?.requestWiFiConfig()
-            else -> { // Note the block
-                print("x is neither 1 nor 2")
-            }
-        }
     }
 
 }
